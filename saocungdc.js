747687374e92e85ce8f19b6c196bb1420ed4b0ed
feazfeaz <bansaohuy333@gmail.com>
2021-01-18 21:13:37 +0700

    180121

---
+++ saocungdc.js
@@ -0,0 +1,12 @@
+console.log("hè lố");
+// var request = new XMLHttpRequest();
+// request.open("GET", 'https://feaz-ws2.herokuapp.com/monius')
+// request.onload = () => {
+//     var data = JSON.parse(request.responseText)
+//     console.log(data)
+// }
+// request.send();
+$(selector).load("url", "data", function (response, status, request) {
+    this; // dom element
+
+});
